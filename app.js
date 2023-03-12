c8533f2af065cf93ede03498b253c69ee27497f0
samvrny <samvrny@gmail.com>
2023-03-12 15:58:55 -0500

    got the main functionality configured

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 let vm = Vue.createApp({
     data() {
         return {
-
+            perspective: 100,
+            rotateY: 0,
+            rotateX: 0,
+            rotateZ: 0
         }
     },
     methods: {
-
+        reset() {
+            this.perspective = 100
+            this.rotateX = 0
+            this.rotateY = 0
+            this.rotateZ = 0
+        }
     },
     computed: {
-
+        changes() {
+            return {
+                transform: `
+                perspective(${this.perspective}px)
+                rotateX(${this.rotateX}deg)
+                rotateY(${this.rotateY}deg)
+                rotateZ(${this.rotateZ}deg)
+                `
+            }
+        }
     }
 }).mount('#app')
